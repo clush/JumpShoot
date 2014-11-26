7610eaf4ee607a9c37a85234f794ce50bd1b091d
Claus <clausburkhart@gmx.de>
2014-11-26 10:06:24 +0100

    Applet 1.1

---
+++ JumpShoot/src/jumpshoot/StartingClass.java
@@ -0,0 +1,164 @@
+package jumpshoot;
+
+import java.applet.Applet;
+import java.awt.Color;
+import java.awt.Frame;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.net.URL;
+
+public class StartingClass extends Applet implements Runnable, KeyListener {
+
+	private static final long serialVersionUID = 1L;
+	private Robot robot;
+	private Image image, background, character;
+	private URL base;
+	private Graphics second;
+
+	@Override
+	public void init() {
+		// TODO Auto-generated method stub
+		setSize(800, 480);
+		setBackground(Color.WHITE);
+		setFocusable(true);
+		addKeyListener(this);
+		Frame frame = (Frame) this.getParent().getParent();
+		frame.setTitle("JumpShoot Alpha");
+		try {
+			base = getDocumentBase();
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+
+		// Bildeinstellung
+		character = getImage(base, "data/character.png");
+		
+	}
+
+	@Override
+	public void start() {
+		robot = new Robot();
+
+		// TODO Auto-generated method stub
+		Thread thread = new Thread(this);
+		thread.start();
+	}
+
+	@Override
+	public void stop() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		while (true) {
+			robot.update();
+			repaint();
+
+			try {
+				Thread.sleep(17);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+		}
+
+	}
+
+	@Override
+	public void update(Graphics g) {
+		// TODO Auto-generated method stub
+		if (image == null) {
+			image = createImage(this.getWidth(), this.getHeight());
+			second = image.getGraphics();
+		}
+
+		second.setColor(getBackground());
+		second.fillRect(0, 0, getWidth(), getHeight());
+		second.setColor(getForeground());
+		paint(second);
+
+		g.drawImage(image, 0, 0, this);
+
+	}
+
+	@Override
+	public void paint(Graphics g) {
+		// TODO Auto-generated method stub
+		g.drawImage(background, 0,0,this);
+		g.drawImage(character, robot.getCenterX() - 61,robot.getCenterY() - 63, this);
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		switch (e.getKeyCode()) {
+		case KeyEvent.VK_UP:
+			System.out.println("Bewege dich hoch");
+			break;
+
+		case KeyEvent.VK_DOWN:
+			System.out.println("Bewege dich runter");
+			break;
+
+		case KeyEvent.VK_LEFT:
+			robot.moveLeft();
+			character = getImage(base, "data/character_links.png");
+			break;
+
+		case KeyEvent.VK_RIGHT:
+			robot.moveRight();
+			character = getImage(base, "data/character.png");
+
+			break;
+
+		case KeyEvent.VK_SPACE:
+			robot.jump();
+			break;
+		}
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		switch (e.getKeyCode()) {
+		case KeyEvent.VK_UP:
+			System.out.println("Stop dich zu bewegen");
+			break;
+
+		case KeyEvent.VK_DOWN:
+			System.out.println("Stop dich zu bewegen");
+			break;
+
+		case KeyEvent.VK_LEFT:
+			robot.stop();
+			break;
+
+		case KeyEvent.VK_RIGHT:
+			robot.stop();
+			break;
+
+		case KeyEvent.VK_SPACE:
+			System.out.println("Stop zu springen");
+			break;
+		}
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		
+	}
+
+}
