c70642e21673865c674545ce568fd2655a9cfd6e
Claus <clausburkhart@gmx.de>
2014-12-09 11:23:43 +0100

    final version 2

---
+++ application/views/index/index.php
@@ -0,0 +1,10 @@
+<div class="content">
+    <h1>Welcome,</h1>
+
+    <!-- echo out the system feedback (error and success messages) -->
+    <?php $this->renderFeedbackMessages(); ?>
+
+    <h2>
+        here you can register and login for our game "JUMP-SHOOT"!
+    </h2>
+</div>
