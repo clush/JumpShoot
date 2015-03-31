62b07617fafafaff9e5e21748c6eef70d7e02ca6
Felix <dersoundhunter@gmx.de>
2015-03-31 10:04:50 +0200

    Update
    Project update at semester start

---
+++ application/views/dashboard/index.php
@@ -0,0 +1,10 @@
+<div class="content">
+    <h1>Start playing!</h1>
+
+    <!-- echo out the system feedback (error and success messages) -->
+    <?php $this->renderFeedbackMessages(); ?>
+
+    <form action="../pong">
+        <input type="submit" value="PLAY" >
+    </form>
+</div>
---
+++ tests/acceptance/Username1Cept.php
@@ -0,0 +1,22 @@
+<?php 
+	$I = new AcceptanceTester($scenario);
+	$I->wantTo('edit username');
+	$I->amOnUrl('http://jump-shoot.it.dh-karlsruhe.de');
+	$I->amOnPage('/login/index');
+	$I->fillField('user_name','Max');
+	$I->fillField('user_password','123456');
+	$I->click('submit');
+	$I->amOnPage('/login/editusername');
+	$I->fillField('user_name','Peter');
+	$I->click('submit');
+	$I->see('Your username has been changed successfully.');
+	$I->amOnPage('/login/logout');
+	$I->amOnPage('/login/index');
+	$I->fillField('user_name','Peter');
+	$I->fillField('user_password','123456');
+	$I->click('submit');
+	$I->amOnPage('/login/editusername');
+	$I->fillField('user_name','Peter');
+	$I->click('submit');
+	$I->see('Sorry, that username is the same as your current one.');
+?>
