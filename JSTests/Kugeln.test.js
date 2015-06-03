4095b5ed9bf24f04b6ef7ee185d76b0d5f73380b
Claus <clausburkhart@gmx.de>
2015-06-03 09:38:10 +0200

    JUMP-56 - Test Programme

---
+++ JSTests/Kugeln.test.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Claus on 20.05.2015.
+ */
+
+describe("Kugeln", function(){
+    it("Treffer gelandet", function() {
+
+        Spieler1.x=40;
+        Spieler1.y=379;
+        Spieler1.kugel1.x=42;
+        Spieler1.kugel1.y=400;
+        Spieler1.kugel1.collision();
+        expect(Spieler1.livebar.length).toEqual(80);
+    });
+});
